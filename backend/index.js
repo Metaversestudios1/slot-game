94248bb22d7cb89faf82419267b05b5adc385a4c
nikita <nikita@gmail.com>
2024-09-17 18:07:29 +0530

    index file changes

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,26 @@ connectDB();
 
 // Middleware
 app.use(cors());
+const corsOption = {
+  origin: "http://localhost:3001",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  credentials: true,
+  allowedHeaders: "Content-Type,Authorization",
+  optionsSuccessStatus: 204
+}
+ 
+app.options('*', cors(corsOption));
+// Enable CORS with specific options
+app.use(cors(corsOption)); 
 app.use(express.json()); // <-- Add this line to parse JSON request bodies
 
+
+app.use('/', async (req, res) => {
+  
+  res.json('ok');
+
+});
 // Routes
 app.get("/api/getHello", (req, res) => {
   res.status(200).json({ message: "hi this is testing server" });
